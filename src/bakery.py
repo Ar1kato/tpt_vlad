7817b890a58c5629ec8391078d5b6b9cbc4b68f9
Aleksandr Kannel <70576249+Ar1kato@users.noreply.github.com>
2020-09-22 21:54:01 +0300

    Update bakery.py

--- src/bakery.py
+++ src/bakery.py
@@ -1,32 +1,5 @@
-class Pastry:
-    """Pastry class."""
-
-    def __init__(self, name: str, complexity_level: int):
-        """Class constructor."""
-        self.name = name
-        self.complexity_level = complexity_level
-
-    def __repr__(self):
-        """Represent object in string format."""
-        return self.name
-
-
-class Baker:
-    """Class baker."""
-
-    def __init__(self, name: str, experience_level: int, money: int):
-        """Class constructor."""
-        self.name = name
-        self.experience_level = experience_level
-        self.money = money
-
-    def __str__(self):
-        """Represent object in string format."""
-        return "Baker: {}({})".format(self.name, self.experience_level)
-
-    def __repr__(self):
-        """Represent object in string format."""
-        return "Baker: {}({})".format(self.name, self.experience_level)
+from src.Baker import Baker
+from src.Pastry import Pastry
 
 class Bakery:
     """Class bakery."""
